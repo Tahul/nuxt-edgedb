e8d6e9f12e8553ce4a59f443ea10bf7d97d62361
Yaël Guilloux <yael.guilloux@gmail.com>
2024-02-08 18:14:45 +0100

    feat(auth): improve auth plugin

--- src/runtime/plugins/edgedb-auth.ts
+++ src/runtime/plugins/edgedb-auth.ts
@@ -8,16 +8,20 @@ export default defineNuxtPlugin(async (nuxtApp) => {
   const cookie = useCookie('edgedb-auth-token')
 
   const updateIdentity = async (event?: H3Event) => {
-    if (!cookie.value)
-      return
-    if (process.server && event) {
-      const req = getRequestURL(event)
-      const url = `${req.protocol}//${req.host}/api/auth/identity`
-      identity.value = await fetchWithEvent(event, url).then(r => r.json())
+    if (!process.server) {
+      identity.value = await $fetch('/api/auth/identity')
       return
     }
-    identity.value = await $fetch('/api/auth/identity')
+
+    if (!cookie.value)
+      return
+
+    const req = getRequestURL(event)
+    const url = `${req.protocol}//${req.host}/api/auth/identity`
+    identity.value = await fetchWithEvent(event, url).then(r => r.json())
   }
+
+  const isLoggedIn = computed(() => !!((identity as Ref<User>)?.value))
 
   const logout = async (redirectTo: string) => {
     await $fetch('/api/auth/logout')
@@ -28,12 +32,14 @@ export default defineNuxtPlugin(async (nuxtApp) => {
 
   if (process.server) {
     const event = nuxtApp?.ssrContext?.event
+
     if (event)
       await updateIdentity(event)
   }
 
   return {
     provide: {
+      edgeDbIsLoggedIn: isLoggedIn,
       edgeDbCookie: cookie,
       edgeDbIdentity: identity,
       edgeDbUpdateIdentity: updateIdentity,
