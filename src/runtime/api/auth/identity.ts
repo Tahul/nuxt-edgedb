97dee3d2cdc1ffaf000628863a6153c39dcd64d7
Yaël Guilloux <yael.guilloux@gmail.com>
2024-02-09 17:23:18 +0100

    fix(auth): fix user creation

--- src/runtime/api/auth/identity.ts
+++ src/runtime/api/auth/identity.ts
@@ -1,7 +1,9 @@
 import { defineEventHandler, deleteCookie, getCookie } from 'h3'
-import { useEdgeDb } from '../../server'
+import { useEdgeDb, useEdgeDbEnv } from '../../server'
 
 export default defineEventHandler(async (req) => {
+  const { identityModel } = useEdgeDbEnv()
+
   const token = getCookie(req, 'edgedb-auth-token')
 
   if (!token) {
@@ -11,5 +13,16 @@ export default defineEventHandler(async (req) => {
 
   const client = useEdgeDb(req)
 
-  return await client.querySingle(`select global current_user;`)
+  let identityTarget = await client.querySingle(`select global current_user;`)
+
+  if (!identityTarget && token) {
+    identityTarget = await client.query(`
+      insert ${identityModel} {
+        name := '',
+        identity := global ext::auth::ClientTokenIdentity
+      }
+    `)
+  }
+
+  return identityTarget
 })
